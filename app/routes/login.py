53459e72b92fdac213088dd10ef68bc6984a8dd0
whrksp126 <whrksp126@gmail.com>
2024-07-29 21:04:04 +0900

    save

--- app/routes/login.py
+++ app/routes/login.py
@@ -52,11 +52,10 @@ def login_google():
 
 
 # 인증 콜백 라우트: OAuth2 인증 완료 후 실행
-@login_bp.route('/login_google/callback')
-def authorize_google():
+@login_bp.route('/login_google/android_callback')
+def authorize_google_android():
     # 상태(state)를 가져옴
     state = session.pop('oauth_state', None)
-
     # 사용자가 리디렉션된 후에 받은 정보를 가져옵니다.
     authorization_response = request.url
 
@@ -130,19 +129,87 @@ def authorize_google():
     return redirect(redirect_url)
 
 
-    # # return jsonify({'name': user.name, 'email': user.email}), 200
 
-    # # 프론트엔드로 리디렉션 URL 생성
-    # front_end_url = 'https://voca.ghmate.com/html/login.html'
-    # query_params = {
-    #     'token': token['access_token'],
-    #     'email': user.email,
-    #     'name': user.name,
-    #     'status': 200
-    # }
-    # print("@#$#@$#", query_params)
-    # redirect_url = f"{front_end_url}?{urlencode(query_params)}"
-    # return redirect(redirect_url)
+
+
+
+# 인증 콜백 라우트: OAuth2 인증 완료 후 실행
+@login_bp.route('/login_google/web_callback')
+def authorize_google_web():
+    # 상태(state)를 가져옴
+    state = session.pop('oauth_state', None)
+    # 사용자가 리디렉션된 후에 받은 정보를 가져옵니다.
+    authorization_response = request.url
+
+    print(f"State from session: {state}")
+    print(f"State from request: {request.args.get('state')}")
+
+    if state is None or state != request.args.get('state'):
+        return 'Invalid OAuth state', 400
+
+    # 사용자가 인증을 마치고 리디렉션 된 후, 코드를 얻어서 토큰을 교환합니다.
+    oauth = OAuth2Session(OAUTH_CLIENT_ID, redirect_uri=OAUTH_REDIRECT_URI, state=state, 
+                          scope=[
+                                'https://www.googleapis.com/auth/userinfo.profile',
+                                'https://www.googleapis.com/auth/userinfo.email',
+                                'openid',
+                                'https://www.googleapis.com/auth/drive.file'
+                              ]
+                        )
+    try:
+
+        token = oauth.fetch_token(
+            'https://accounts.google.com/o/oauth2/token',
+            authorization_response=authorization_response,
+            client_secret=OAUTH_CLIENT_SECRET
+        )
+        print("@@@token", token)
+
+        # 토큰에서 사용자 정보 추출
+        userinfo = oauth.get('https://www.googleapis.com/oauth2/v1/userinfo').json()
+        print("@@@userinfo", userinfo)
+    except Exception as e:
+        print(f"Error during token fetch or userinfo fetch: {str(e)}")
+        return f"An error occurred: {str(e)}", 500
+    
+    # 토큰 정보를 세션에 저장
+    session['token'] = token
+
+    # 사용자 정보 확인
+    user = User.query.filter_by(google_id=userinfo['id']).first()
+    
+    print('@@@@@useruserinfo',userinfo)
+    if user is None:
+        print("comming!!!")
+        print("userinfo", userinfo['id'])
+        # 사용자가 존재하지 않으면 회원가입 처리
+        new_user = User(
+            email=userinfo['email'],
+            google_id=userinfo['id'],
+            name=userinfo.get('name', ''),
+            phone=None
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        user = new_user
+
+    # 사용자 정보를 세션에 저장
+    session['user_id'] = user.id
+    login_user(user)
+
+    # return jsonify({'name': user.name, 'email': user.email}), 200
+
+    # 프론트엔드로 리디렉션 URL 생성
+    front_end_url = 'https://voca.ghmate.com/html/login.html'
+    query_params = {
+        'token': token['access_token'],
+        'email': user.email,
+        'name': user.name,
+        'status': 200
+    }
+    print("@#$#@$#", query_params)
+    redirect_url = f"{front_end_url}?{urlencode(query_params)}"
+    return redirect(redirect_url)
 
 
 @login_bp.route("/logout")
