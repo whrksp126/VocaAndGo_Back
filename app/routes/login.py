f368e9c7fceaa65f331812ee8071ccbbaecbc049
whrksp126 <whrksp126@gmail.com>
2024-08-01 23:37:32 +0900

    save

--- app/routes/login.py
+++ app/routes/login.py
@@ -129,11 +129,11 @@ def authorize_google():
         return redirect(redirect_url)
     elif device_type == 'app':
         # Expo 앱으로 리디렉션 URL 생성
-        # expo_redirect_url = 'exp://192.168.0.63:8081/--/auth'
+        app_redirect_url = 'exp://192.168.0.63:8081/--/auth'
         # 프로덕션 앱으로 리디렉션 URL 생성
         # app_redirect_url = 'vocaandgo_app://auth'
         # app_redirect_url = 'https://voca.ghmate.com/html/login.html'
-        app_redirect_url = 'HeyVoca://auth'
+        # app_redirect_url = 'HeyVoca://auth'
 
         query_params = {
             'token': token['access_token'],
