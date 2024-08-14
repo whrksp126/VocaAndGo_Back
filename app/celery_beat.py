6a0ecac73543bba5763cf7475fa84e0bb4681146
zyzyzyclaire <kunji117@naver.com>
2024-08-14 20:51:56 +0900

    샐러리테스트

--- app/celery_beat.py
+++ app/celery_beat.py
@@ -1,13 +1,19 @@
 from celery import Celery
-from celery.schedules import crontab
+from celery.schedules import crontab, timedelta
 from celery import Celery
 from celery_config import celery
 
 # Celery 비트 스케줄 설정
+# celery.conf.beat_schedule = {
+#     'send-fcm-every-day-at-8am': {
+#         'task': 'celery.send_fcm_message',
+#         'schedule': crontab(hour=8, minute=0),  # 매일 오전 08:00
+#     },
+# }
 celery.conf.beat_schedule = {
-    'send-fcm-every-day-at-8am': {
+    'send-fcm-every-10-seconds': {
         'task': 'celery.send_fcm_message',
-        'schedule': crontab(hour=8, minute=0),  # 매일 오전 08:00
+        'schedule': timedelta(seconds=10),  # 10초마다 실행
     },
 }
 celery.conf.timezone = 'UTC'
