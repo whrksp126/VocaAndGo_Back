b827c92d8120e4e67aeb7ec73cb081035c5eb16b
zyzyzyclaire <kunji117@naver.com>
2024-09-19 03:06:13 +0900

    오늘의문장

---
+++ app/routes/mainpage.py
@@ -0,0 +1,28 @@
+from flask import render_template, redirect, url_for, request, session, jsonify
+from app import db
+from app.routes import mainpage_bp
+from app.models.models import User, DailySentence
+from datetime import datetime, timedelta
+from sqlalchemy import func
+
+from flask_login import current_user, login_required, login_user, logout_user
+
+import io
+import json
+
+
+# TODO: item 없으면 어떻게 보내줄까용?
+@mainpage_bp.route('/')
+@login_required
+def send_daily_sentence():
+    today = (datetime.utcnow() + timedelta(hours=9)).date()
+    item = DailySentence.query\
+                    .filter(func.date(DailySentence.date) == today)\
+                    .first()
+
+    res = {
+        'sentence': item.sentence,
+        'meaning': item.meaning
+    }
+    
+    return res
