3484e83c9d2af61516284e5e7c56014b9e33e1c1
A-Hyun <gih1214@hanmail.net>
2024-09-19 21:48:37 +0900

    서점/단어 모델 추가 및 수정

--- app/models/models.py
+++ app/models/models.py
@@ -6,6 +6,7 @@ from sqlalchemy.types import String, Integer, Date, DateTime, Boolean, Text, Big
 
 from sqlalchemy.dialects.mysql import BINARY
 from sqlalchemy.types import TypeDecorator
+from sqlalchemy.orm import relationship
 
 from uuid import uuid4, UUID
 from datetime import datetime, timedelta
@@ -55,26 +56,110 @@ class User(db.Model):
     def is_authenticated(self):
         return True
 
+##############
+# 기본 테이블 #
+##############
+
+# 단어장
+class VocaBook(db.Model):
+    __tablename__ = 'voca_book'
+    id = Column(Integer, primary_key=True)
+    book_nm = Column(String(255), nullable=False)
+    language = Column(String(50), nullable=False)
+    source = Column(String(100), nullable=False)
+    category = Column(String(100), nullable=True)
+    username = Column(String(100), nullable=True)
+    word_count = Column(Integer, nullable=True)
+    updated_at = Column(DateTime, nullable=True)
+
+    # 관계 정의
+    voca_books = relationship("VocaBookMap", back_populates="voca_book")
+
 
 # 단어
-class Word(db.Model):
-    __tablename__ = 'word'
+class Voca(db.Model):
+    __tablename__ = 'voca'
     id = Column(Integer, primary_key=True)
-    word = Column(String(200), nullable=True)
-    pronunciation = Column(String(200), nullable=True)
-    example = Column(String(500), nullable=True)
-    created_at = Column(DateTime, nullable=True)
-    updated_at = Column(DateTime, nullable=True)
+    word = Column(String(255), nullable=False)
+    pronunciation = Column(String(100), nullable=True)
+
+    # 관계 정의
+    voca_books = relationship("VocaBookMap", back_populates="voca")
+    voca_meanings = relationship("VocaMeaningMap", back_populates="voca")
+    voca_examples = relationship("VocaExampleMap", back_populates="voca")
 
 
-# 단어뜻
-class Meaning(db.Model):
-    __tablename__ = 'meaning'
+# 단어 뜻
+class VocaMeaning(db.Model):
+    __tablename__ = 'voca_meaning'
     id = Column(Integer, primary_key=True)
-    meaning = Column(String(200), nullable=True)
-    created_at = Column(DateTime, nullable=True)
-    updated_at = Column(DateTime, nullable=True)
-    word_id = Column(Integer, ForeignKey('word.id'), nullable=True)
+    meaning = Column(String(255), nullable=False)
+
+    # 관계 정의
+    meanings_voca = relationship("VocaMeaningMap", back_populates="meaning")
+
+
+# 단어 예문
+class VocaExample(db.Model):
+    __tablename__ = 'voca_example'
+    id = Column(Integer, primary_key=True)
+    exam_en = Column(Text, nullable=True)
+    exam_ko = Column(Text, nullable=True)
+
+    # 관계 정의
+    examples_voca = relationship("VocaExampleMap", back_populates="example")
+
+
+# 서점
+class Bookstore(db.Model):
+    __tablename__ = 'bookstore'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), nullable=False)
+    downloads = Column(Integer, nullable=False)
+    category = Column(String(255), nullable=False)
+    color = Column(String(255), nullable=True)
+    hide = Column(String(1), nullable=False)
+    book_id = Column(Integer, ForeignKey('voca_book.id'), nullable=False)
+
+    # 관계 정의
+    voca_book = relationship("VocaBook")
+
+
+##############
+# 관계 테이블 #
+##############
+
+# 단어장-단어
+class VocaBookMap(db.Model):
+    __tablename__ = 'voca_book_map'
+    voca_id = Column(Integer, ForeignKey('voca.id', ondelete='CASCADE', onupdate='NO ACTION'), primary_key=True)
+    book_id = Column(Integer, ForeignKey('voca_book.id', ondelete='CASCADE', onupdate='NO ACTION'), primary_key=True)
+
+    # 관계 정의
+    voca = relationship("Voca", back_populates="voca_books")
+    voca_book = relationship("VocaBook", back_populates="books_voca")
+
+
+# 단어뜻-단어
+class VocaMeaningMap(db.Model):
+    __tablename__ = 'voca_meaning_map'
+    voca_id = Column(Integer, ForeignKey('voca.id', ondelete='CASCADE', onupdate='NO ACTION'), primary_key=True)
+    meaning_id = Column(Integer, ForeignKey('voca_meaning.id', ondelete='CASCADE', onupdate='NO ACTION'), primary_key=True)
+
+    # 관계 정의
+    voca = relationship("Voca", back_populates="voca_meanings")
+    meaning = relationship("VocaMeaning", back_populates="meanings_voca")
+
+
+# 단어예문-단어
+class VocaExampleMap(db.Model):
+    __tablename__ = 'voca_example_map'
+    voca_id = Column(Integer, ForeignKey('voca.id', ondelete='CASCADE', onupdate='NO ACTION'), primary_key=True)
+    example_id = Column(Integer, ForeignKey('voca_example.id', ondelete='CASCADE', onupdate='NO ACTION'), primary_key=True)
+
+    # 관계 정의
+    voca = relationship("Voca", back_populates="voca_examples")
+    example = relationship("VocaExample", back_populates="examples_voca")
 
 
 class DailySentence(db.Model):
