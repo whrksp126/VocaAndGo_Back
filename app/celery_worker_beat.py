8a6bf63689ee7685cba4c7222efb589a11c3f015
zyzyzyclaire <kunji117@naver.com>
2024-11-07 23:49:47 +0900

    temp

--- app/celery_worker_beat.py
+++ app/celery_worker_beat.py
@@ -3,6 +3,8 @@ from celery.schedules import crontab, timedelta
 import logging
 import os
 
+from app import create_app
+from app.routes.fcm import send_notification
 
 # Celery 설정
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
@@ -29,60 +31,13 @@ logging.basicConfig(filename='celery.log', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 
-
-from pyfcm import FCMNotification
-from app import db
-from app.models.models import db, User, UserHasToken
-import json
-
-# FCM API 키 (Firebase Console에서 확인 가능)
-push_service = FCMNotification(service_account_file='app/config/vocaandgo-firebase-adminsdk-xyi9u-e4f0ccc423.json',
-                                 project_id='vocaandgo')
-
-
-# FCM 메시지 전송 함수
-def send_push_notification(title, message, token):
-    result = push_service.notify(fcm_token=token, 
-                                notification_title=title, 
-                                notification_body=message, 
-                                notification_image=None
-                            )
-
-    return result
-
-
-# 메시지 전송 API
-def send_notification():
-    # title = request.json.get('title')
-    # message = request.json.get('message')
-    title = '팀이 꼭...(더보기)'
-    message = '팀이 꼭 이 메시지를 봤으면 좋겠다...'
-
-    try:
-        # # DB에서 저장된 토큰 조회
-        # cursor.execute("SELECT token FROM fcm_tokens")
-        # tokens = cursor.fetchall()
-
-        tokens = db.session.query(UserHasToken).all()
-
-        # 모든 토큰에 푸시 알림 전송
-        results = []
-        for token in tokens:
-            result = send_push_notification(title, message, token.token)
-            results.append(result)
-
-        print("fcm success!")
-        return json.dumps({"results": results}), 200
-    except Exception as e:
-        print("fcm failed : ", e)
-        return json.dumps({"error": str(e)}), 500
-
-
 # Celery 작업 정의
 @celery.task
 def send_fcm_message():
-    send_notification()     # fcm 함수 실행
-    logger.info('Successfully sent message:')
+    app = create_app()
+    with app.app_context():
+        send_notification()     # fcm 함수 실행
+
 
 # Celery Beat 스케줄 설정
 celery.conf.beat_schedule = {
