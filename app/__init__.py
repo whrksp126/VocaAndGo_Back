5ccb505b8a380a41db3cd0545af09b0f93b6ded9
gh jo <whrksp126@gmail.com>
2024-11-08 12:01:14 +0900

    save

--- app/__init__.py
+++ app/__init__.py
@@ -15,7 +15,6 @@ db = SQLAlchemy()
 migrate = Migrate()     
 login_manager = LoginManager()
 
-print("!@#!@#!@#!@#!@#!@#!@#!@#@!#!@")
 
 # def start_scheduler():
 #     print("스케줄러 comming")
@@ -26,6 +25,75 @@ print("!@#!@#!@#!@#!@#!@#!@#!@#@!#!@")
 #     scheduler.add_job(send_notification, 'interval', minutes=1)  # 1분마다 실행
 #     # scheduler.add_job(send_notification, 'cron', hour=22, minute=0) # 10시마다
 #     scheduler.start()
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.triggers.cron import CronTrigger
+import atexit
+
+
+# FCM API 키 (Firebase Console에서 확인 가능)
+push_service = FCMNotification(service_account_file='app/config/vocaandgo-firebase-adminsdk-xyi9u-e4f0ccc423.json',
+                                 project_id='vocaandgo')
+
+
+# FCM 메시지 전송 함수
+def send_push_notification(title, message, token):
+    result = push_service.notify(fcm_token=token, 
+                                notification_title=title, 
+                                notification_body=message, 
+                                notification_image=None
+                            )
+
+    return result
+
+
+def send_fcm_message():
+    with app.app_context():  # Flask 애플리케이션 컨텍스트 내에서 실행
+
+
+        from app.models.models import db, User, UserHasToken
+
+        print("1시에 FCM 메시지를 전송합니다.")
+        # 여기에 FCM 메시지 전송 로직 작성
+
+        # # 메시지 전송 API
+        title = '두번쨰... 메시지'
+        message = '잠온다'
+
+        try:
+            # # DB에서 저장된 토큰 조회
+            # cursor.execute("SELECT token FROM fcm_tokens")
+            # tokens = cursor.fetchall()
+
+            tokens = db.session.query(UserHasToken).all()
+
+            # 모든 토큰에 푸시 알림 전송
+            results = []
+            for token in tokens:
+                try:
+                    result = send_push_notification(title, message, token.token)
+                    results.append(result)
+                except Exception as e:
+                    print(f"Error sending to token {token.token}: {e}")
+                    results.append({"error": str(e), "token": token.token})
+
+            print("fcm success!")
+            return json.dumps({"results": results}), 200
+        except Exception as e:
+            print("fcm failed : ", e)
+            return json.dumps({"error": str(e)}), 500
+
+
+def create_scheduler():
+    print('@@@@@')
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(send_fcm_message, IntervalTrigger(minutes=1))  # 매 1분마다 실행
+    scheduler.start()
+    atexit.register(lambda: scheduler.shutdown())
+    return scheduler
+
+# 애플리케이션 시작 시 스케줄러도 시작
+scheduler = create_scheduler()
 
 
 def create_app():
