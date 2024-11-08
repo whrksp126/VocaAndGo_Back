0051722fe920e66b55455aecaf52d2d7e55d6fcd
gh jo <whrksp126@gmail.com>
2024-11-08 12:16:10 +0900

    save

--- app/__init__.py
+++ app/__init__.py
@@ -8,28 +8,15 @@ from contextlib import contextmanager
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine, text
 from flask_cors import CORS
-# from apscheduler.schedulers.background import BackgroundScheduler
-
 import json
 
 db = SQLAlchemy()
 migrate = Migrate()     
 login_manager = LoginManager()
 
-
-# def start_scheduler():
-#     print("스케줄러 comming")
-#     scheduler = BackgroundScheduler(daemon=True)
-    
-#     # send_notification 함수를 실행할 때만 fcm.py를 임포트
-#     from app.routes.fcm import send_notification
-#     scheduler.add_job(send_notification, 'interval', minutes=1)  # 1분마다 실행
-#     # scheduler.add_job(send_notification, 'cron', hour=22, minute=0) # 10시마다
-#     scheduler.start()
-
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
-from apscheduler.triggers.interval import IntervalTrigger  # IntervalTrigger 임포트 추가
+from apscheduler.triggers.interval import IntervalTrigger 
 
 import atexit
 from pyfcm import FCMNotification
@@ -54,15 +41,13 @@ def send_push_notification(title, message, token):
 def send_fcm_message(app):
     with app.app_context():  # Flask 애플리케이션 컨텍스트 내에서 실행
 
-
         from app.models.models import db, User, UserHasToken
 
-        print("1시에 FCM 메시지를 전송합니다.")
         # 여기에 FCM 메시지 전송 로직 작성
 
         # # 메시지 전송 API
-        title = '두번쨰... 메시지'
-        message = '잠온다'
+        title = 'TEST'
+        message = 'testtest'
 
         try:
             # # DB에서 저장된 토큰 조회
@@ -89,9 +74,8 @@ def send_fcm_message(app):
 
 
 def create_scheduler(app):
-    print('@@@@@')
     scheduler = BackgroundScheduler()
-    scheduler.add_job(lambda: send_fcm_message(app), IntervalTrigger(minutes=1))  # 매 1분마다 실행
+    scheduler.add_job(lambda: send_fcm_message(app), CronTrigger(hour=14, minute=0))
     scheduler.start()
     atexit.register(lambda: scheduler.shutdown())
     return scheduler
@@ -109,19 +93,11 @@ def create_app():
     migrate.init_app(app, db)
     login_manager.init_app(app)
     # login_manager.login_view = "main_login.html"
-
-    # print("-----스케줄러 시작-----")
-    # # APScheduler 설정
-    # start_scheduler()
-    # print("-----스케줄러 끝-----")
-
     
     # # 모든 모델 클래스들을 한번에 import
     from app.models import models
     from app.login_manager import load_user, unauthorized_callback
     
-    # with app.app_context():
-    #     db.create_all()    
     from app.routes.login import login_bp
     from app.routes.search import search_bp
     from app.routes.tts import tts_bp
