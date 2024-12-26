6c4be4d264ccfef3db48841ad426f54e9c3738c1
zyzyzyclaire <kunji117@naver.com>
2024-12-27 01:48:59 +0900

    temp

--- app/login_manager.py
+++ app/login_manager.py
@@ -21,18 +21,18 @@ from app import login_manager # Flask-login의 변수
 
 
 
-# # 사용자 로드 함수
-# @login_manager.user_loader
-# def load_user(user_id):
-#     from app.models.models import db, User
+# 사용자 로드 함수
+@login_manager.user_loader
+def load_user(user_id):
+    from app.models.models import db, User
     
-#     print("@#$load_user")
-#     user_item = db.session.query(User).filter(User.id == user_id).first()
-#     return user_item
+    print("@#$load_user")
+    user_item = db.session.query(User).filter(User.id == user_id).first()
+    return user_item
 
-# # 로그인이 되어있지 않은 경우
-# @login_manager.unauthorized_handler
-# def unauthorized_callback():
-#     print("@#$unauthorized")
-#     print('로그인이 되어있지 않은 경우')
-#     return "로그인이 필요합니다.", 401
+# 로그인이 되어있지 않은 경우
+@login_manager.unauthorized_handler
+def unauthorized_callback():
+    print("@#$unauthorized")
+    print('로그인이 되어있지 않은 경우')
+    return "로그인이 필요합니다.", 401
